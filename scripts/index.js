fe873c707882bce0524d54948263762c1f7d17bb
Ilya Shustenkov <siv_2003@mail.ru>
2024-02-14 18:32:24 +0300

    v1

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,24 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const container = document.querySelector('.content');
+const cardConatainer = container.querySelector('.places__list');
+
+function removeCard(evt) {
+  evt.target.parentElement.remove();
+}
+
+function addCard(name, link, callback) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+  cardElement.querySelector('.card__title').textContent = name;
+  cardElement.querySelector('.card__image').src = link;
+
+  cardElement.querySelector('.card__delete-button').addEventListener('click', callback);
+
+  return cardElement;
+}
+
+initialCards.forEach((element) => cardConatainer.append(addCard(element.name, element.link, removeCard)));
